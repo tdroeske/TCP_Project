5927fdb147bd01f9af98380946346c66fdbf8e15
root <root@PLAYBOBB.(none)>
2014-10-01 02:07:28 -0400

    Can now send syn packet using mysocket interface. Began making connection set up.

--- TCPServer.py
+++ TCPServer.py
@@ -7,6 +7,7 @@ print 'The server is ready to receive'
 while 1:
 	connectionSocket, addr = serverSocket.accept()
 	sentence = connectionSocket.recv(1024)
+	print "Recieved: " + sentence
 	capitalizedSentence = sentence.upper()
 	connectionSocket.send(capitalizedSentence)
 	connectionSocket.close()
